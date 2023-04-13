8dd1c7a3ace3339e439c6f5532e3e862e3d1cf8d
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-13 19:43:45 +0200

    add general configure site

--- vite-dc-comics/src/App.vue
+++ vite-dc-comics/src/App.vue
@@ -10,53 +10,235 @@ export default {
 </script>
 
 <template>
+  <header>
+    <div class="container">
+      <div class="row justify-content-between align-items-baseline">
+        <div class="col">
+          <div class="logo p-2">
+            <img src="./assets/img/dc-logo.png" alt="" srcset="">
+          </div>
+        </div>
+        <div class="col ">
+          <nav class="navbar navbar-expand-sm navbar-light py-0">
+            <div class="container-fluid">
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="CHARACTER">CHARACTER</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="COMICS">COMICS</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="MOVIES">MOVIES</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="TV">TV</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="GAMES">GAMES</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="COLLECTIBLES">COLLECTIBLES</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="VIDEOS">VIDEOS</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="FANS">FANS</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="NEWS">NEWS</a>
+              <a class="py-4 px-1 text-decoration-none text-black p-1" href="SHOP">SHOP</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
 
-  <header></header>
+
+  </header>
 
   <main>
-    <section >
-
-    </section>
-    <nav class="navbar navbar-expand-sm navbar-light ">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">primary</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
-                    aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarID">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        
-                    </div>
-                </div>
-            </div>
-    </nav>
+    <div class="container">
+      <div class="row flex-column">
+        <div class="col">
+          <section>
+            <h1>Contenet goes here</h1>
+          </section>
+        </div>
+      </div>
+    </div>
   </main>
 
   <footer>
-    <nav class="navbar navbar-expand-sm navbar-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">primary</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
-                    aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarID">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        
-                    </div>
+    <section class="top text-white m-auto">
+      <div class="container">
+        <div class="row flex-column">
+          <div class="col">
+            <nav class="navbar navbar-expand-sm shop p-3">
+              <div class="container-fluid ">
+                <div class="py-3">
+                  <a class="navbar-brand" href="#">
+                    <img class="m-2" src="./assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                  </a>
                 </div>
+                <div class="py-3">
+                  <a class="navbar-brand" href="#">
+                  <img class="m-1" src="./assets/img/buy-comics-merchandise.png" alt="">
+                  <span>DC MERCHANDISE</span>
+                </a>
+                </div>
+                
+                <div class="py-3">
+                  <a class="navbar-brand" href="#">
+                  <img class="m-1" src="./assets/img/buy-comics-subscriptions.png" alt="">
+                  <span>SUBSCRIPTION</span>
+                </a>
+                </div>
+
+                
+                <div class="py-3">
+                  <a class="navbar-brand" href="#">
+                  <img class="m-1" src="./assets/img/buy-comics-shop-locator.png" alt="">
+                  <span>COMIC SHOP LOCATOR</span>
+                </a>
+                </div>
+
+                
+                <div class="py-3">
+                  <a class="navbar-brand" href="#">
+                  <img class="m-1" src="./assets/img/buy-dc-power-visa.svg" alt="">
+                  <span>DC POWER VISA</span>
+                </a>
+                </div>
+
+                
+              </div>
+            </nav>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <section class="center p-3">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <nav class="navbar navbar-expand-sm contacts">
+              <div class="container-fluid p-0">
+                <section class="py-1 d-flex flex-column h-100">
+                  <h4>DC COMICS</h4>
+                  <ul class="px-0 list-unstyled">
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Characters</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Comics</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Movies</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">TV</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Games</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Video</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">News</a>
+                    </li>
+                  </ul>
+                </section>
+                <section class="py-1 d-flex flex-column h-100">
+                  <h4>SHOP</h4>
+                  <ul class="px-0 list-unstyled">
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Shop DC</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Shop DC Collecribles</a>
+                    </li>
+                  </ul>
+                </section>
+                <section class="py-1 d-flex flex-column h-100">
+                  <h4>DC</h4>
+                  <ul class="px-0 list-unstyled">
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Advertising</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Jobs</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Ratings</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Shop Help</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">Contact Us</a>
+                    </li>
+                  </ul>
+                </section>
+                <section class="py-1 d-flex flex-column h-100">
+                  <h4>SITES</h4>
+                  <ul class="px-0 list-unstyled">
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">DC</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">DC Kinds</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">DC Universe</a>
+                    </li>
+                    <li>
+                      <a class="text-decoration-none text-white" href="#">DC Power Visa</a>
+                    </li>
+                  </ul>
+                </section>
+
+
+                <a class="navbar-brand" href="#">primary</a>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bottom py-1">
+      <div class="container py-2">
+        <div class="row justify-content-between py-4">
+          <div class="col">
+            <a name="" id="" class="btn text-white border-1 border-primary" href="#" role="button">SIGN-UP NOW!</a>
+          </div>
+          <div class="col d-flex justify-content-end">
+            <div class="followUs d-flex py-2 ">
+              <h4 class="text-white px-3 d-flex align-items-center">Follow US</h4>
+              <div class="icon">
+                <img class="m-1" src="./assets/img/footer-facebook.png" alt="">
+                <img class="m-1" src="./assets/img/footer-twitter.png" alt="">
+                <img class="m-1" src="./assets/img/footer-youtube.png" alt="">
+                <img class="m-1" src="./assets/img/footer-pinterest.png" alt="">
+                <img class="m-1" src="./assets/img/footer-periscope.png" alt="">
+              </div>
             </div>
-    </nav>
-    <section>
+          </div>
+        </div>
+      </div>
 
     </section>
-  </footer>
 
+  </footer>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
